e1d26d5fa8a47578def52601318aaeef0eb5727e
Jinyu <cadenho@hotmail.com>
2024-04-04 20:22:01 +0800

    remove used resources

--- Scripts and data/parse_hanzi.py
+++
@@ -1,8 +0,0 @@
-root = "../HSK Hanzi/"
-level = "HSK 2.txt"
-
-with open(f"{root}{level}", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-    for line in lines:
-        line = [char for char in line]
-        for char in line:
